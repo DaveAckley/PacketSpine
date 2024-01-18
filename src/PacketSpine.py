073219c6eaef86342eda6712e2319f40819a4d6f
Dave Ackley <ackley@livingcomputation.org>
2024-01-18 14:11:54 -0700

    Autocalibrate looplen

--- src/PacketSpine.py
+++ src/PacketSpine.py
@@ -14,22 +14,26 @@ class PacketSpine:
         self.nonce = random.randint(1,254)
         self.loopLen = 0    # assume nobody's out there at first
         self.flagSPacketSeen() # assume the loop's good to start
+        self.flagMPacketSeen() # assume the loop's good to start
 
     def flagSPacketSeen(self):
         self.sawSPacket = True 
+
+    def flagMPacketSeen(self):
+        self.sawMPacket = True 
         
     def sendSPacket(self,hops,dest):
-        packetFormat = b'%cW%c%cS0'
+        packetFormat = b'%cW%c%cS0123A123B123C123D123E123F123G123H123I123J123K123L123M123N123O123P'
         packet = packetFormat % (Utils.intToSignedByte(hops),
                                  Utils.intToSignedByte(dest),
                                  self.nonce)
         self.sendPacket(packet)
         
     def initSM(self): # Buffer up all outbound S packets for a simulation step
-        if not self.sawSPacket:
-            self.loopLen += 1
-            print("LOOPLEN NOW",self.loopLen)
+        #if not self.sawSPacket and self.sawMPacket:
+        #    self.updateLoopLengthIfNeeded(self.loopLen + 1)
         self.sawSPacket = False
+        self.sawMPacket = False
 
         if self.nonce >= 255:
             self.nonce = 1
@@ -44,6 +48,11 @@ class PacketSpine:
             hops = self.loopLen - i - 1
             dest = hops
             self.sendSPacket(hops,dest)
+
+    def updateLoopLengthIfNeeded(self,length):
+        if length != self.loopLen:
+            print("LOOPLENGTH UPDATE:",self.loopLen,"->",length)
+            self.loopLen = length
 
     def registerPacketProcessor(self,pproc):
         return self.sl.registerPacketProcessor(pproc)
